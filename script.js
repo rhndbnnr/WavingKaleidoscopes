aceec325773a362f1121d27d2ae027936c92fad3
rhndbnnr <rhndbnnr@gmail.com>
2024-03-13 12:43:26 -0600

    Form Info with Arias

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById('commentForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    
+   const formData = new FormData(this);
+   const name = formData.get('name');
+   const email =formData.get('email');
+   const message =formData.get('message');
+   
